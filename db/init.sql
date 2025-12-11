a81e87a9038c67a29b8d6e6361ef4e2bde543ee9
liljoker06 <lahrimialadin@gmail.com>
2025-12-11 13:07:36 +0100

    feat: Add CSV import functionality for movies into the database
    - Implemented a script to load movie data from a CSV file.
    - Added functions to convert data types and handle CSV reading.
    - Established a connection to the PostgreSQL database for inserting movie records.
    - Included error handling for duplicate entries using ON CONFLICT clause.
    - Utilized JSON for storing additional metadata related to each movie.
    - Enhanced logging for successful and skipped entries during the import process.

--- db/init.sql
+++ db/init.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE,
     age INT,
     gender VARCHAR(50),
+    password VARCHAR(255) NOT NULL,
     preferences JSONB,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -22,6 +23,7 @@ CREATE TABLE IF NOT EXISTS users (
 -- =========================
 CREATE TABLE IF NOT EXISTS contents (
     content_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    tmdb_id BIGINT UNIQUE,                      -- 🔥 Nouveau
     title VARCHAR(255) NOT NULL,
     description TEXT,
     type VARCHAR(100),
@@ -30,7 +32,7 @@ CREATE TABLE IF NOT EXISTS contents (
     duration INT,
     creator_id VARCHAR(255),
     upload_date DATE,
-    language VARCHAR(50),
+    language VARCHAR(255),
     popularity_score DOUBLE PRECISION DEFAULT 0,
     metadata JSONB,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
